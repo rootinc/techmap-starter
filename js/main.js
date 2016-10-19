90dc5a8949a8b59fa7cb1f4d96de8100aef169ce
ajstrand <ajstrand8@gmail.com>
2016-10-19 14:51:52 -0400

    made changes based on Dan's feedback and reorganized some of the logic

--- js/main.js
+++ js/main.js
@@ -1,47 +1,46 @@
 var techMap = {};
 $(document).ready(function(){
   techMap.pages = $(".page");
-
-  techMap.currentPage = 0;
-
-  for(var i = 0;i<techMap.pages.length;i++){
-    var page = $(techMap.pages[i]);
-    page.hide();
-  }
-
+  var pagesActualLength = techMap.pages.length - 1;
+  var beginningPage = 0;
   var that = this;
 
+  techMap.currentPage = 0;
   techMap.el = $(document);
-  
+
+  techMap.pages.hide();
+
   techMap.el.hammer().on('swiperight', function () {
-      previous();
+    var nextPage = techMap.currentPage + 1;
+    previous(nextPage, beginningPage);
   });
 
   techMap.el.hammer().on('swipeleft', function () {
-      next();
-  });
-   start();
-});
-  function start(){
-    $(techMap.pages[techMap.currentPage]).show();
-  }
-  function next(){
-    var pagesActualLength = techMap.pages.length - 1;
     var previousPage = techMap.currentPage - 1;
-    $(techMap.pages[techMap.currentPage]).hide();
-    techMap.currentPage++;
-    if(techMap.currentPage > pagesActualLength){
-      techMap.currentPage = previousPage;
-    }
-    $(techMap.pages[techMap.currentPage]).show();
+    next(previousPage, pagesActualLength);
+  });
+
+  start();
+});
+
+function start(){
+  $(techMap.pages[techMap.currentPage]).show();
+}
+
+function next(previousPage, pagesActualLength){
+  $(techMap.pages[techMap.currentPage]).hide();
+  techMap.currentPage++;
+  if(techMap.currentPage > pagesActualLength){
+    techMap.currentPage = previousPage;
   }
-  function previous(){
-    var beginningPage = 0;
-    var nextPage = techMap.currentPage + 1;
-    $(techMap.pages[techMap.currentPage]).hide();
-    techMap.currentPage--;
-    if(techMap.currentPage < beginningPage){
-      techMap.currentPage = nextPage;
-    }
-    $(techMap.pages[techMap.currentPage]).show();
+  $(techMap.pages[techMap.currentPage]).show();
+}
+
+function previous(nextPage, beginningPage){
+  $(techMap.pages[techMap.currentPage]).hide();
+  techMap.currentPage--;
+  if(techMap.currentPage < beginningPage){
+    techMap.currentPage = nextPage;
   }
+  $(techMap.pages[techMap.currentPage]).show();
+}
