95fc3aadd5c388ca333e0e64d71ae4b4b6e7dbd8
tomihu <thuss@rootinc.com>
2017-06-05 10:52:44 -0400

    Comment/clean up.

--- _harp/js/activities/TripleChartSliderActivity.js
+++ _harp/js/activities/TripleChartSliderActivity.js
@@ -24,44 +24,35 @@ var TripleChartSliderActivity = Activity.extend({
   {
     var that = this;
     var handles = this.page.$el.find(this.handleSelector);
+    // for each handle clip....
     handles.each(function (n, o){
-      console.log(" Handle " , o);
       var handle = $(o);
       handle.draggable({
         axis: "x", 
         containment:"parent",
-        start:function(){
-          
-        },
         drag:function(e, ui){
+          // find the handle, and the "bar" associated with it.
           var active_handle = $(e.target);
           var level = active_handle.parent().find(".level");
+          // Find left, and original left (which is max)
           var left = ui.position.left;
-          var max = active_handle.parent().width() - active_handle.width();
-          var oleft = max;
-          /*
-          if (active_handle.data("oleft")){
-            oleft = active_handle.data("oleft");
-          } else {
-            active_handle.data("oleft", ui.originalPosition.left);
-            oleft = active_handle.data("oleft");
-          }
-          console.log("\nLevel:",level);
-          console.log(left , " / " , oleft)
-          console.log("Dragging " , e, " : " , ui)
-          */
+          var oleft = active_handle.parent().width() - active_handle.width();
+          // Find values for the blue bar.
           var level_width = oleft - left;
           var level_perc = Math.floor(level_width / oleft*100)/100;
           level.css("width", level_width);
         },
         start:function(e, ui){
+          // find the bar, and the instructions associated with it.
           var active_handle = $(e.target);
           var instruct = active_handle.parent().find(".instruct");
+          // prevent page advancing.
           that.page.module.canSwipe = false;
           instruct.fadeOut(100);
         },
         stop:function(){
           that.submitButton.show();
+          // turn page advancing back on.
           that.page.module.canSwipe = true;
          } 
       })
@@ -69,9 +60,12 @@ var TripleChartSliderActivity = Activity.extend({
     this.submitButton.on("click", function(){
       that.submitButton.fadeOut(100).delay(100).hide();
       //
+      // make sure all the instruction are hidden - 
+      // even if not all the bars were moved.
       var instructs = that.page.$el.find(".instruct");
       instructs.fadeOut(100);
       //
+      // animate the handles to the correct positions.
       handles.each(function (n, o){
         console.log(" Handle " , o);
         var active_handle = $(o);
@@ -80,14 +74,15 @@ var TripleChartSliderActivity = Activity.extend({
           var max = active_handle.parent().width() - active_handle.width();
           var oleft = max;
           var level_width = oleft - answer;
-        level.animate({
-          width: level_width,
-        },2000);
-        active_handle.animate({
-          left: answer,
-        },2000);
+          level.animate({
+            width: level_width,
+          },2000);
+          active_handle.animate({
+            left: answer,
+          },2000);
       });
       
     })
   },
+  
 });
